516b4debbdd0a710337366f99301bae448ab303e
Ofelia Rahmanova <ofelyaar@code.edu.az>
2021-04-08 15:41:40 +0400

    Added 'Customers' page

---
+++ src/pages/Customers.js
@@ -0,0 +1,31 @@
+import map from '../assets/img/map.png'
+import { useContext } from 'react'
+import { DataContext } from '../DataContext'
+import MainSection from '../components/Home/MainSection'
+
+const Customers = () => {
+    const { companyData } = useContext(DataContext);
+    const [companies] = companyData;
+
+    return (
+        <div id="customers-page">
+            <MainSection>
+                <div className="customers">
+                    <div className="map-container">
+                        <img src={map} alt="map" />
+                    </div>
+                    <div className="companies-list">
+                        {companies.map(company =>
+                            <div className="company-item" key={company.id}>
+                                <img src={company.logo} alt={company.name} />
+                                <h6>{company.name}</h6>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </MainSection>
+        </div>
+    );
+}
+
+export default Customers;
